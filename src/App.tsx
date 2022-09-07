d7f4d128b4a17005d436e449a4517a8a39b89c4d
aromanarguello <aromanarguello@gmail.com>
2022-09-06 21:27:38 -0400

    Add react-router and chakra

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import { Home } from './routes/defaultRoutes';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>Hello</p>
-      </header>
-    </div>
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
   );
 }
 
---
+++ src/styles/theme.ts
@@ -0,0 +1,8 @@
+import { extendTheme } from '@chakra-ui/react';
+
+export const theme = extendTheme((styles) => ({
+  ...styles,
+  // components: {
+  //   Button: ButtonStyles,
+  // },
+}));
