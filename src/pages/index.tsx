17b9b6ca5c41258f02f275295f4cfb3a6e6a3177
aromanarguello <aromanarguello@gmail.com>
2022-09-06 21:11:46 -0400

    initial commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const IndexPage = () => (
+  <div className="App">
+    <header className="App-header">
+      <p>Hello World</p>
+    </header>
+  </div>
+);
+
+export default IndexPage;
